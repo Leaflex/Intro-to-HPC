f6731c8604500249f56eccacdaff76a8396ce1d1
LexSomers <lex.somers@fiveq.com>
2024-09-17 12:00:22 -0500

    created c and python code folders. helloWorld.c test program

---
+++ c_code/helloWorld.c
@@ -0,0 +1,12 @@
+// Compile: gcc helloWorld.c -o helloWorld
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    if (argc > 1) {
+        printf("Hello, %s!\n", argv[1]);
+    } else {
+        printf("Hello, world!\n");
+    }
+    
+    return 0;
+}
