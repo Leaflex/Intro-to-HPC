5c10deba7e8ffedf402e532dc3cc6aac5843e887
LexSomers <lex.somers@fiveq.com>
2024-10-23 21:25:31 -0500

    Completed lab15 and 17. Screenshots for sbatch job in lab17 folder

---
+++ c_code/Lab17/omp_hello.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#ifdef _OPENMP
+#include <omp.h>
+#endif
+
+//#include <omp.h>
+//@nova.its.iastate.edu
+
+int main () {
+    void hello ();
+
+    printf("\n");
+
+#   pragma omp parallel
+    hello ();
+
+    printf("\n");
+}
+
+void hello () {
+    # ifdef _OPENMP
+        const int my_rank = omp_get_thread_num ();
+        const int actual_thread_count = omp_get_num_threads ();
+    # else
+        const int my_rank = 0;
+        const int actual_thread_count = 1;
+    # endif
+        printf(" Hello from thread %i of %i\n", my_rank +1, actual_thread_count );
+}
